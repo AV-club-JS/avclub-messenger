f7a5c0bc6652763a4505fef36591671b35d2c22f
Andrey Kunev <akunev45@gmail.com>
2023-11-22 16:26:45 +0200

    Added CTA component

---
+++ avclub-msngr/src/components/CTA/CTA.tsx
@@ -0,0 +1,65 @@
+import {
+    Box,
+    Heading,
+    Container,
+    Text,
+    Button,
+    Stack,
+} from '@chakra-ui/react'
+
+export const CTA = () => {
+    return (
+        <>
+            <Container maxW={'3xl'}>
+                <Stack
+                    as={Box}
+                    textAlign={'center'}
+                    spacing={{ base: 8, md: 14 }}
+                    py={{ base: 20, md: 36 }}>
+                    <Heading
+                        fontWeight={600}
+                        fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+                        lineHeight={'110%'}>
+                        Instant, Intuitive, Irreplaceable<br />
+                        <Text as={'span'} color={'blue.400'}>
+                        Unlock the Future of Team Collaboration with AV Messenger
+                        </Text>
+                    </Heading>
+                    <Text color={'gray.500'}>
+                        Monetize your content by charging your most loyal readers and reward them
+                        loyalty points. Give back to your loyal readers by granting them access to
+                        your pre-releases and sneak-peaks.
+                    </Text>
+                    <Stack
+                        direction={'column'}
+                        spacing={3}
+                        align={'center'}
+                        alignSelf={'center'}
+                        position={'relative'}>
+                        <Button
+                            colorScheme={'green'}
+                            bg={'blue.400'}
+                            rounded={'full'}
+                            px={6}
+                            _hover={{
+                                bg: 'blue.500',
+                            }}>
+                            Register
+                        </Button>
+                        <Text color={'gray.500'} fontWeight={600} fontSize={'md'}>Already have an account?</Text>
+                        <Button
+                            colorScheme={'blue'}
+                            bg={'blue.400'}
+                            rounded={'full'}
+                            px={6}
+                            _hover={{
+                                bg: 'blue.500',
+                            }}>
+                            Login
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Container>
+        </>
+    )
+}
