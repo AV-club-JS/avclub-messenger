136fd7e69ab112452b853277f864142f20d0c595
Andrey Kunev <akunev45@gmail.com>
2023-11-22 18:49:33 +0200

    Styling added icons to Features in Home

--- avclub-msngr/src/components/Features/Features.tsx
+++ avclub-msngr/src/components/Features/Features.tsx
@@ -10,7 +10,7 @@ import {
     Icon,
     useColorModeValue,
 } from '@chakra-ui/react';
-import { IoAnalyticsSharp, IoSearchSharp } from 'react-icons/io5';
+import { AiOutlineMessage, AiOutlineTeam, AiOutlineVideoCamera } from "react-icons/ai";
 import { SingleFeature } from './SingleFeature';
 
 export const Features = () => {
@@ -18,7 +18,7 @@ export const Features = () => {
         <Container maxW={'5xl'} py={12}>
             <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
                 <Stack spacing={4}>
-                    <Heading>Empower Your Team with AV Messenger</Heading>
+                    <Heading fontWeight={1000}>Empower Your Team with AV Messenger</Heading>
                     <Text color={'gray.500'} fontSize={'lg'}>
                         Transform How You Communicate: Seamless Collaboration, Ultimate Connectivity!
                     </Text>
@@ -28,18 +28,18 @@ export const Features = () => {
                             <StackDivider borderColor={useColorModeValue('gray.100', 'gray.700')} />
                         }>
                         <SingleFeature
-                            icon={<Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />}
-                            iconBg={useColorModeValue('yellow.100', 'yellow.900')}
+                            icon={<Icon as={AiOutlineTeam} color={'#001f3f'} w={5} h={5} />}
+                            iconBg={useColorModeValue('blue.100', 'blue.900')}
                             text={'Team Up, Plan and Collaborate'}
                         />
                         <SingleFeature
-                            icon={<Icon as={IoSearchSharp} color={'green.500'} w={5} h={5} />}
+                            icon={<Icon as={AiOutlineMessage} color={'#001f3f'} w={5} h={5} />}
                             iconBg={useColorModeValue('green.100', 'green.900')}
                             text={'Upgrade Your Chat Experience'} />
                         <SingleFeature
-                            icon={<Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />}
+                            icon={<Icon as={AiOutlineVideoCamera} color={'#001f3f'} w={5} h={5} />}
                             iconBg={useColorModeValue('purple.100', 'purple.900')}
-                            text={'Stay Connected'}
+                            text={'Elevate Team Communication with Video Conferencing'}
                         />
                     </Stack>
                 </Stack>
@@ -51,6 +51,9 @@ export const Features = () => {
                             'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
                         }
                         objectFit={'cover'}
+                        loading={'eager'}
+                        border={'4px'}  // Add a 1px border
+                        borderColor={'#001f3f'}
                     />
                 </Flex>
             </SimpleGrid>
