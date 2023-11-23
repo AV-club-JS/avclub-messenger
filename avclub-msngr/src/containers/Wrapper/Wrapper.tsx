b2586ed19643792c59f8870d9b3bfecf0e9f28c4
Andrey Kunev <akunev45@gmail.com>
2023-11-23 10:04:30 +0200

    Added return to Wrapper

--- avclub-msngr/src/containers/Wrapper/Wrapper.tsx
+++ avclub-msngr/src/containers/Wrapper/Wrapper.tsx
@@ -1,14 +1,16 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-export const Wrapper = () => (
-  <ChakraProvider>
-    <Box
-      m={0}
-      p={0}
-      minH={'100vh'}
-      width={'100vw'}
-    >
-      <Outlet/>
-    </Box>
-  </ChakraProvider>
-);
+export const Wrapper = () => {
+  return (
+    <ChakraProvider>
+      <Box
+        m={0}
+        p={0}
+        minH={'100vh'}
+        width={'100vw'}
+      >
+        <Outlet />
+      </Box>
+    </ChakraProvider>
+  )
+};
