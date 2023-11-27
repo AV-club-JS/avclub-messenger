8a95bde74578f28e8a18f71880293a78fe3a72bb
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-27 17:58:47 +0200

    Fully implemented Register

--- avclub-msngr/src/containers/Home/Home.tsx
+++ avclub-msngr/src/containers/Home/Home.tsx
@@ -1,9 +1,13 @@
 import { Features } from "../../components/Features";
 import { CTA } from "../../components/CTA";
 import { ActivityBar } from "../../components/ActivityBar";
-
+import { UserContext } from "../../context/AuthContext";
+import { useContext } from "react";
 export const Home = () => {
-
+    const { user, userData, setAuth } = useContext(UserContext) 
+    console.log(`the user is `, user);
+    console.log(userData);
+    console.log(setAuth)
     return (
         <>
             <Features />
