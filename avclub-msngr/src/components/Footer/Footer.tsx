d76c0b6df8e40376f0ad55ae0849ad785fea2cdf
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-27 10:52:01 +0200

    Implemented Footer component

--- avclub-msngr/src/components/Footer/Footer.tsx
+++ avclub-msngr/src/components/Footer/Footer.tsx
@@ -1,33 +1,177 @@
+import {
+  Avatar,
+  Box,
+  CheckboxProps,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
+import { ChildrenProps } from "../../types/types";
 
-import { Box, Container, Stack, Text, useColorModeValue } from '@chakra-ui/react'
+const Testimonial = (props: ChildrenProps) => {
+  const { children } = props;
+
+  return <Box>{children}</Box>;
+};
+
+const TestimonialContent = (props: CheckboxProps) => {
+  const { children } = props;
+
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      boxShadow={"lg"}
+      p={8}
+      rounded={"xl"}
+      align={"center"}
+      pos={"relative"}
+      _after={{
+        content: `""`,
+        w: 0,
+        h: 0,
+        borderLeft: "solid transparent",
+        borderLeftWidth: 16,
+        borderRight: "solid transparent",
+        borderRightWidth: 16,
+        borderTop: "solid",
+        borderTopWidth: 16,
+        borderTopColor: useColorModeValue("white", "gray.800"),
+        pos: "absolute",
+        bottom: "-16px",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      {children}
+    </Stack>
+  );
+};
+
+const TestimonialHeading = (props: ChildrenProps) => {
+  const { children } = props;
+
+  return (
+    <Heading as={"h3"} fontSize={"xl"}>
+      {children}
+    </Heading>
+  );
+};
+
+const TestimonialText = (props: CheckboxProps) => {
+  const { children } = props;
+
+  return (
+    <Text
+      textAlign={"center"}
+      color={useColorModeValue("gray.600", "gray.400")}
+      fontSize={"sm"}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const TestimonialAvatar = ({
+  src,
+  name,
+  title,
+}: {
+  src: string;
+  name: string;
+  title: string;
+}) => {
+  return (
+    <Flex align={"center"} mt={8} direction={"column"}>
+      <Avatar src={src} mb={2} />
+      <Stack spacing={-1} align={"center"}>
+        <Text fontWeight={600}>{name}</Text>
+        <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
+          {title}
+        </Text>
+      </Stack>
+    </Flex>
+  );
+};
+
+const WithSpeechBubbles = () => {
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.700")}>
+      <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+        <Stack spacing={0} align={"center"}>
+          <Heading>Who we are:</Heading>
+          <Text>Connecting people is our pasion</Text>
+        </Stack>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing={{ base: 10, md: 4, lg: 10 }}
+        >
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Intuitive Design</TestimonialHeading>
+              <TestimonialText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+                neque sed imperdiet nibh lectus feugiat nunc sem.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={"https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"}
+              name={"Andrey Kunev"}
+              title={"JavaScript developer, Asairing web developer, Future Game developer"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Mindblowing Service</TestimonialHeading>
+              <TestimonialText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+                neque sed imperdiet nibh lectus feugiat nunc sem.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={"https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"}
+              name={"Velislav Karastoychev"}
+              title={"Econometrician, JavaScript developer"}
+            />
+          </Testimonial>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
 
 export const Footer = () => {
   return (
     <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <WithSpeechBubbles />
       <Container
         as={Stack}
-        maxW={'6xl'}
+        maxW={"6xl"}
         py={4}
-        direction={{ base: 'column', md: 'row' }}
+        direction={{ base: "column", md: "row" }}
         spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}>
-        <Stack direction={'row'} spacing={6}>
-          <Box as="a" href={'#'}>
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Stack direction={"row"} spacing={6}>
+          <Box as="a" href={"#"}>
             Home
           </Box>
-          <Box as="a" href={'#'}>
+          <Box as="a" href={"#"}>
             About
           </Box>
-          <Box as="a" href={'#'}>
+          <Box as="a" href={"#"}>
             Contact us
           </Box>
         </Stack>
         <Text>© 2023 AVL club</Text>
       </Container>
     </Box>
-  )
-}
+  );
+};
