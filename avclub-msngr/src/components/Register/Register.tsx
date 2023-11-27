95f0f35ab249db80cc1ff67a231aea38d3495e11
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-27 17:58:47 +0200

    Fully implemented Register

--- avclub-msngr/src/components/Register/Register.tsx
+++ avclub-msngr/src/components/Register/Register.tsx
@@ -14,6 +14,7 @@ import {
   Stack,
   Text,
   useColorModeValue,
+  useToast,
 } from "@chakra-ui/react";
 import { ChangeEvent, MouseEvent, useContext, useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
