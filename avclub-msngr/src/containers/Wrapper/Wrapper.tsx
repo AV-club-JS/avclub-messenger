6e70a88b78747ee0deae6f49bf8d3467bbbd1fb8
Andrey Kunev <akunev45@gmail.com>
2023-11-28 14:22:31 +0200

    User context now persists

--- avclub-msngr/src/containers/Wrapper/Wrapper.tsx
+++ avclub-msngr/src/containers/Wrapper/Wrapper.tsx
@@ -1,32 +1,65 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UserContext } from '../../context/AuthContext';
 import { AppState } from '../../types/types';
 import { Navbar } from '../../components/Navbar';
 import customTheme from '../../theme/theme';
+import { auth } from '../../config/firebase-config';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getUserByUid } from '../../services';
 
-import { Footer } from '../../components/Footer';
 export const Wrapper = () => {
   const [appState, setAppState] = useState<AppState>({
     user: null,
     userData: null,
   });
+  const [user, loading] = useAuthState(auth);
 
-  return (
-    <ChakraProvider theme={customTheme}>
-      <UserContext.Provider value={{ ...appState, setAuth: setAppState }}>
-        <Box
-          m={0}
-          p={0}
-          minH={'100vh'}
-          w={'100vw'}
-          maxW={'100%'} 
-        >
-          <Navbar />
-          <Outlet />
-        </Box>
-      </UserContext.Provider>
-    </ChakraProvider>
-  )
-};
+  const [userDataLoading, setUserDataLoading] = useState(false);
+
+  if (appState.user !== user) {
+    setAppState({ ...appState, user });
+  }
+
+  useEffect(() => {
+    (async () => {
+      if (user === null) {
+        return;
+      }
+      try {
+        setUserDataLoading(true);
+        const req = await getUserByUid(user.uid);
+        const data = req.val();
+        setAppState({
+          ...appState,
+          userData: { ...data },
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setUserDataLoading(false);
+      }
+    })();
+  }, [user]);
+
+  if (!loading && !userDataLoading) {
+
+    return (
+      <ChakraProvider theme={customTheme}>
+        <UserContext.Provider value={{ ...appState, setAuth: setAppState }}>
+          <Box
+            m={0}
+            p={0}
+            minH={'100vh'}
+            w={'100vw'}
+            maxW={'100%'}
+          >
+            <Navbar />
+            <Outlet />
+          </Box>
+        </UserContext.Provider>
+      </ChakraProvider>
+    )
+  };
+}
