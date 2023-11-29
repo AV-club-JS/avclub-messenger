dcba780657580c7ed2351e226c5d691f10ee51d5
Andrey Kunev <akunev45@gmail.com>
2023-11-29 12:25:17 +0200

    Made bio text editor config more extensive

--- avclub-msngr/src/utils/profileUtils.ts
+++ avclub-msngr/src/utils/profileUtils.ts
@@ -24,5 +24,55 @@ export const validatePhoneNumber = (number: string) => {
 
 export const froalaBioConfig = {
     charCounterCount: true,
-    charCounterMax: 1200,
+    charCounterMax: 8200,
+    attribution: false,
+    "toolbarButtons": {
+        "moreText": {
+            "buttons": [
+                "bold",
+                "italic",
+                "underline",
+                "strikeThrough",
+                "subscript",
+                "superscript",
+                "fontFamily",
+                "fontSize",
+                "textColor",
+                "backgroundColor",
+                "inlineClass",
+                "inlineStyle",
+                "clearFormatting"
+            ],
+            "buttonsVisible": 3,
+            "align": "left"
+        },
+        "moreParagraph": {
+            "buttons": [
+                "markdown",
+                "emoticons",
+                "alignRight",
+                "alignJustify",
+                "formatOL",
+                "formatUL",
+                "paragraphFormat",
+                "paragraphStyle",
+                "lineHeight",
+                "outdent",
+                "indent",
+                "quote"
+            ],
+            "buttonsVisible": 2,
+            "align": "left"
+        },
+        "moreMisc": {
+            "buttons": [
+                "undo",
+                "redo",
+                "fullscreen",
+                "selectAll",
+            ],
+            "buttonsVisible": 2,
+            "align": "right"
+        }
+    }
 }
