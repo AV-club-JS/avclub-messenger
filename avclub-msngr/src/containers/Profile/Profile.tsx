6a2817ad3f53e1459689a7009bb2cb562bfb6bbe
Andrey Kunev <akunev45@gmail.com>
2023-11-29 12:25:34 +0200

    Added import for froala

--- avclub-msngr/src/containers/Profile/Profile.tsx
+++ avclub-msngr/src/containers/Profile/Profile.tsx
@@ -7,6 +7,7 @@ import { getStatusBadgeColor, validatePhoneNumber, froalaBioConfig } from "../..
 // froala editor
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/plugins.pkgd.min.js';
 import FroalaEditorComponent from 'react-froala-wysiwyg';
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
 // context
