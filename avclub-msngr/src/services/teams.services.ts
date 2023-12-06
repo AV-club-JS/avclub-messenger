cd47113f6eebf4142019c9d242431fbfef4e9a8e
Andrey Kunev <akunev45@gmail.com>
2023-12-06 09:59:11 +0200

    Added addUserToTeam, doesTeamNameExist

--- avclub-msngr/src/services/teams.services.ts
+++ avclub-msngr/src/services/teams.services.ts
@@ -14,7 +14,9 @@ import { db } from "../config/firebase-config";
 // types
 import { SetCount, DefaultTeamData, SetTeamData } from "../types/types";
 // constants
-import { TEAMS } from "../constants/servicesConstants";
+import { TEAMS, USERS } from "../constants/servicesConstants";
+// services
+import { updateUserData } from ".";
 
 const teamsRef = ref(db, `${TEAMS}/`);
 
@@ -49,14 +51,38 @@ export const getTeamInfo = async (teamId: string) => {
 
 export const listenTeamData = (teamId: string, setTeamData: SetTeamData) => {
     const teamRef = ref(db, `${TEAMS}/${teamId}`);
-    return onValue(teamRef, (snapshot) => {
-        const data = snapshot.val();
-        setTeamData(data);
-    })
+    try {
+        const unsubscribe = onValue(teamRef, (snapshot) => {
+            const data = snapshot.val();
+            setTeamData(data);
+        });
+        return unsubscribe;
+    } catch (error) {
+        return () => {};
+    }
 }
 
 export const updateTeamData = async (uid: string, data: object) => {
     const teamRef = ref(db, `${TEAMS}/${uid}`);
     await update(teamRef, data);
+    return true;
+}
+
+export const addUserToTeam = async (userId: string, teamId: string) => {
+    const teamRef = ref(db, `${TEAMS}/${teamId}/members`);
+    const userRef = ref(db, `${USERS}/${userId}/teamIds`);
+    const dataForTeam = { [`${userId}`]: true };
+    const dataForUser = { [`${teamId}`]: true};
+    await update(teamRef, dataForTeam);
+    await update(userRef, dataForUser);
+}
+
+export const doesTeamNameExist = async (teamName: string) => {
+    const req = await get(query(teamsRef, orderByChild('name'), equalTo(teamName)));
+    const data = req.val();
+    console.log(data);
+    if (data === null) {
+        return false;
+    }
     return true;
 }
