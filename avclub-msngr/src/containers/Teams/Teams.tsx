d55f93eb8edf6461164244a61a00cd35430fc7d1
Andrey Kunev <akunev45@gmail.com>
2023-12-06 15:41:20 +0200

    Made updating dynamic

--- avclub-msngr/src/containers/Teams/Teams.tsx
+++ avclub-msngr/src/containers/Teams/Teams.tsx
@@ -1,49 +1,63 @@
-import { Stack, Heading, Divider, Button, Text } from "@chakra-ui/react";
+import { Stack, Heading, Divider, Button, useDisclosure } from "@chakra-ui/react";
 import { useEffect, useState, useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { UserContext } from "../../context/AuthContext";
 import { DefaultTeamData } from "../../types/types";
 import { TeamsDisplay } from "../../components/TeamsDisplay";
-import { getTeamInfo } from "../../services";
+import { getTeamInfo, removeUserTeam } from "../../services";
+import { CreateTeam } from "../../components/CreateTeam";
 
 
 export const Teams = () => {
     const { userData } = useContext(UserContext);
     const [teamsArr, setTeamsArr] = useState<DefaultTeamData[]>([]);
+    const { isOpen: isCreateOpen, onOpen: onCreateOpen, onClose: onCreateClose } = useDisclosure();
+    const [loading, setLoading] = useState(true);
 
+    const handleCreateOpen = () => {
+        onCreateOpen();
+    };
 
     useEffect(() => {
         if (userData && userData.teamIds) {
             (async () => {
                 const teamKeys = Object.keys(userData.teamIds!);
                 const teams: DefaultTeamData[] = [];
-                
+
                 for (const key of teamKeys) {
                     const data = await getTeamInfo(key);
-                    teams.push(data);
+
+                    if (data) {
+                        teams.push(data);
+                    } else {
+                        await removeUserTeam(userData.uid, key);
+                    }
                 }
-                
+
                 setTeamsArr(teams);
+                setLoading(false);
             })()
         }
     }, [userData]);
 
-    if (userData) {
+    if (userData && !loading) {
         return (
             <Stack direction='row' gap={1} h={`calc(100vh - 60px)`} overflowY={'hidden'}>
-                <Stack direction='column' 
-                bgColor='white' 
-                borderRight='1px solid'
-                borderColor='gray.100' 
-                w='23%'>
+                <Stack direction='column'
+                    bgColor='white'
+                    borderRight='1px solid'
+                    borderColor='gray.100'
+                    w='23%'>
                     <Button w='50%' m={2}
+                        fontSize='20'
                         color={'brand.primary'}
                         variant={'ghost'}
-                        fontSize='20'
                         _hover={{
                             bg: 'brand.primary',
                             color: 'brand.accent',
-                        }}>Create Team</Button>
+                        }}
+                        onClick={handleCreateOpen}>Create Team</Button>
+                    <CreateTeam isOpen={isCreateOpen} onClose={onCreateClose} />
                     <Heading m={3}>Teams</Heading>
                     <Divider />
                     <Stack direction={'column'} overflow={'auto'} ml={4}>
@@ -51,7 +65,7 @@ export const Teams = () => {
                     </Stack>
                 </Stack>
                 <Stack direction='column' w={'77%'}>
-                    <Outlet/>
+                    <Outlet />
                 </Stack>
             </Stack>
         )
