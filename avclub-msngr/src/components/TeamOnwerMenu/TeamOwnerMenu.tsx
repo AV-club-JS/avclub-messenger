b0fbf6ae8864960f538445f8437071188fa6aef8
Andrey Kunev <akunev45@gmail.com>
2023-12-06 16:43:58 +0200

    Added confirmation to delete team

--- avclub-msngr/src/components/TeamOnwerMenu/TeamOwnerMenu.tsx
+++ avclub-msngr/src/components/TeamOnwerMenu/TeamOwnerMenu.tsx
@@ -1,32 +1,60 @@
 import { Button, Stack } from "@chakra-ui/react";
 import { TeamButtonsProps } from "../../types/types";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 
 export const TeamOwnerMenu: FC<TeamButtonsProps> = ({ handleDelete, handleSearchOpen }) => {
+    const [confirmDelete, setConfirmDelete] = useState(false);
+
     return (
-            <Stack direction="row" spacing={4} justify="center" p={4} borderBottom="1px solid" borderColor="gray.200">
-                <Button
+        <Stack direction="row" spacing={4} justify="center" p={4} borderBottom="1px solid" borderColor="gray.200">
+            <Button
+                w={{ base: 'auto', md: '10%' }}
+                fontSize='sm'
+                color={'brand.primary'}
+                variant={'ghost'}
+                _hover={{
+                    bg: 'brand.primary',
+                    color: 'brand.accent',
+                }}
+                onClick={() => handleSearchOpen()}>
+                Add Member
+            </Button>
+            {confirmDelete ?
+                <>
+                    <Button
+                        w={{ base: 'auto', md: '10%' }}
+                        fontSize='sm'
+                        color={'red'}
+                        variant={'ghost'}
+                        _hover={{
+                            bg: 'red',
+                            color: 'brand.primary',
+                        }}
+                        onClick={() => handleDelete()}>Confirm</Button>
+                    <Button
+                        w={{ base: 'auto', md: '10%' }}
+                        fontSize='sm'
+                        color={'brand.primary'}
+                        variant={'ghost'}
+                        _hover={{
+                            bg: 'brand.primary',
+                            color: 'brand.accent',
+                        }}
+                        onClick={() => setConfirmDelete(false)}>Cancel</Button>
+                </>
+                : <Button
                     w={{ base: 'auto', md: '10%' }}
                     fontSize='sm'
-                    color={'brand.primary'}
-                    variant={'ghost'}
-                    _hover={{
-                        bg: 'brand.primary',
-                        color: 'brand.accent',
-                    }}
-                    onClick={() => handleSearchOpen()}>
-                    Add Member
-                </Button>
-                <Button
-                    w={{ base: 'auto', md: '10%' }}
                     color={'red'}
                     variant={'ghost'}
                     _hover={{
                         bg: 'red',
                         color: 'brand.primary',
                     }}
-                    onClick={() => handleDelete()}>Delete Team</Button>
-            </Stack>
+                    onClick={() => setConfirmDelete(true)}>Delete Team</Button>
+            }
+
+        </Stack>
     )
 }
