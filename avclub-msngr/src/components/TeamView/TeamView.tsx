23c94fd5d5eacb77c2771790554f36003a31dc46
Andrey Kunev <akunev45@gmail.com>
2023-12-06 09:59:57 +0200

    Added info and edit options

--- avclub-msngr/src/components/TeamView/TeamView.tsx
+++ avclub-msngr/src/components/TeamView/TeamView.tsx
@@ -1,12 +1,20 @@
-import { Text, Flex, Stack, Heading, Divider, Button, FormControl, Center, Input } from "@chakra-ui/react";
+import { Text, Flex, Stack, Heading, Divider, Button, FormControl, Center, Input, useDisclosure, FormErrorMessage } from "@chakra-ui/react";
 import { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../../context/AuthContext";
 import { DefaultTeamData, DefaultUserData } from "../../types/types";
-import { listenTeamData, getUserByUid, getUsersByTeam, updateTeamData } from "../../services";
+import { listenTeamData, getUserByUid, getUsersByTeam, updateTeamData, doesTeamNameExist } from "../../services";
 import { Unsubscribe } from "firebase/database";
 import { formatTimestamp } from "../../utils/formatTimestamp";
 import { TeamMembers } from "../TeamMembers";
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/plugins.pkgd.min.js';
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
+import { froalaBioConfig } from "../../utils/profileUtils";
+import { AddMemberModal } from "../AddMemberModal";
+
 
 export const TeamView = () => {
     const { teamId: urlTeamId } = useParams();
@@ -16,21 +24,53 @@ export const TeamView = () => {
     const { userData } = useContext(UserContext);
     const [isEditingTeamName, setIsEditingTeamName] = useState(false);
     const [teamName, setTeamName] = useState('');
+    const [teamInfo, setTeamInfo] = useState('');
+    const [isEditingInfo, setIsEditingInfo] = useState(false);
+    const [teamNameInvalid, setTeamNameInvalid] = useState(false);
+    const [teamNameError, setTeamNameError] = useState('');
+    const { isOpen: isSearchOpen, onOpen: onSearchOpen, onClose: onSearchClose } = useDisclosure();
+
+    const handleSearchOpen = () => {
+        onSearchOpen();
+    };
+
 
     const handleTeamNameEdit = async () => {
+
         if (teamName.length < 5) {
-            alert('Team name must be at least five symbols!');
-            setIsEditingTeamName(false);
+            setTeamNameInvalid(true);
+            setTeamNameError('Team name must be at least five symbols.');
+            console.log(teamNameInvalid);
+            console.log(teamNameError);
             return;
         }
-
         try {
+            const check = await doesTeamNameExist(teamName);
+            if (teamName == teamData.name) {
+                return;
+            }
+            if (check == true) {
+                setTeamNameInvalid(true);
+                setTeamNameError('This name already exists.');
+                return;
+            }
             await updateTeamData(teamData.teamId, { name: teamName });
+            console.log(check);
+            setTeamNameInvalid(false);
             setIsEditingTeamName(false);
         } catch (error) {
             console.error(error);
         }
 
+    }
+
+    const handleInfoEdit = async () => {
+        try {
+            await updateTeamData(teamData.teamId, { info: teamInfo });
+            setIsEditingInfo(false);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     useEffect(() => {
@@ -39,11 +79,12 @@ export const TeamView = () => {
                 try {
                     const req = await getUserByUid(teamData.owner);
                     const ownerData = req.val();
+                    setOwner(ownerData.username);
                     const membersData = await getUsersByTeam(teamData.teamId);
 
-                    setOwner(ownerData.username);
-                    setMembers(membersData);
-
+                    setMembers([...membersData]);
+                    setTeamName(teamData.name);
+                    setTeamInfo(teamData.info);
                 } catch (error) {
                     console.error(error);
                 }
@@ -58,30 +99,42 @@ export const TeamView = () => {
         } catch (error) {
             console.error(error);
 
-            return () => {
-                disconnectTeamListener();
-            }
+        }
+        return () => {
+            setIsEditingTeamName(false);
+            setIsEditingInfo(false);
+            setTeamNameError('');
+            disconnectTeamListener();
         }
     }, [urlTeamId])
 
     if (teamData && owner) {
         return (
-            <Flex direction="column" px={4} mt={4}>
+            <Stack direction="column" px={4} mt={4}>
                 <Stack direction='column' align='center'>
                     <Stack alignItems='center'>
-                        <FormControl>
-                            {isEditingTeamName ?
-                                <Input
-                                    value={teamName}
-                                    onChange={e => setTeamName(e.target.value)}></Input>
-                                : <Heading>{teamData.name}</Heading>}
+                        <FormControl isInvalid={teamNameInvalid}>
+                            {isEditingTeamName ? (
+                                <>
+                                    <Input m={1} value={teamName} onChange={(e) => setTeamName(e.target.value)} />
+                                    <FormErrorMessage>{teamNameError}</FormErrorMessage>
+                                </>
+                            ) : (
+                                <>
+                                    <Heading>{teamData.name}</Heading>
+                                    {teamNameInvalid && <FormErrorMessage>{teamNameError}</FormErrorMessage>}
+                                </>
+                            )}
                             <Center>
-                                {(userData!.uid === teamData.owner) && (isEditingTeamName ?
-                                    <Button size='sm'
-                                        onClick={handleTeamNameEdit}>Save</Button>
-                                    : <Button size='sm'
-                                        onClick={() => setIsEditingTeamName(true)}>
-                                        Edit</Button>)}
+                                {userData!.uid === teamData.owner && (isEditingTeamName ? (
+                                    <Button size="sm" onClick={handleTeamNameEdit}>
+                                        Save
+                                    </Button>
+                                ) : (
+                                    <Button size="sm" onClick={() => setIsEditingTeamName(true)}>
+                                        Edit
+                                    </Button>
+                                ))}
                             </Center>
                         </FormControl>
                     </Stack>
@@ -90,12 +143,34 @@ export const TeamView = () => {
                     <Text fontSize='sm' color='gray.400'>ID:{teamData.teamId}</Text>
                     <Divider />
                 </Stack>
-                <Stack direction='column' alignItems='left' m={6}>
-                    <Text fontWeight={600}>Members:</Text>
-                    <TeamMembers users={members} />
-                    {(userData!.uid === teamData.owner) && <Button w={'10%'} fontSize='sm'>Add Member</Button>}
+                <Stack direction='row' alignItems='left' m={6}>
+                    <Stack direction='column'>
+                        <Text fontWeight={600}>Members:</Text>
+                        <TeamMembers users={members} />
+                    </Stack>
+                    <Stack direction='column' ml={3}>
+                        <Stack direction='row'>
+                            <Text fontWeight={600}>Team Info:</Text>
+                            {(userData!.uid === teamData.owner) && (isEditingInfo ?
+                                <Button size='sm'
+                                    onClick={handleInfoEdit}>Save</Button>
+                                : <Button size='sm'
+                                    onClick={() => setIsEditingInfo(true)}>
+                                    Edit</Button>)}
+                        </Stack>
+                        {isEditingInfo ?
+                            <FroalaEditorComponent
+                                model={teamInfo}
+                                onModelChange={(e: string) => setTeamInfo(e)}
+                                config={froalaBioConfig} />
+                            : <FroalaEditorView model={teamInfo} />}
+                    </Stack>
                 </Stack>
-            </Flex>
+                {(userData!.uid === teamData.owner) && <Button w={'10%'} fontSize='sm' onClick={handleSearchOpen}>
+                    Add Member
+                </Button>}
+                <AddMemberModal isOpen={isSearchOpen} onClose={onSearchClose} teamId={urlTeamId!} />
+            </Stack>
         )
     }
 }
