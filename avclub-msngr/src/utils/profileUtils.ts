133b1b9d73fab608964372931856bfc989d2a2f4
Andrey Kunev <akunev45@gmail.com>
2023-12-07 22:14:23 +0200

    Added message config for froala

--- avclub-msngr/src/utils/profileUtils.ts
+++ avclub-msngr/src/utils/profileUtils.ts
@@ -77,3 +77,59 @@ export const froalaBioConfig = {
         }
     }
 }
+
+export const froalaMessageConfig = {
+    charCounterCount: false,
+    heightMax: '15vh',
+    attribution: false,
+    quickInsertEnabled: false,
+    "toolbarButtons": {
+        "moreText": {
+            "buttons": [
+                "bold",
+                "italic",
+                "underline",
+                "strikeThrough",
+                "subscript",
+                "superscript",
+                "fontFamily",
+                "fontSize",
+                "textColor",
+                "backgroundColor",
+                "inlineClass",
+                "inlineStyle",
+                "clearFormatting"
+            ],
+            "buttonsVisible": 3,
+            "align": "left"
+        },
+        "moreParagraph": {
+            "buttons": [
+                "markdown",
+                "emoticons",
+                "alignRight",
+                "alignJustify",
+                "formatOL",
+                "formatUL",
+                "paragraphFormat",
+                "paragraphStyle",
+                "lineHeight",
+                "outdent",
+                "indent",
+                "quote"
+            ],
+            "buttonsVisible": 2,
+            "align": "left"
+        },
+        "moreMisc": {
+            "buttons": [
+                "undo",
+                "redo",
+                "fullscreen",
+                "selectAll",
+            ],
+            "buttonsVisible": 2,
+            "align": "right"
+        }
+    }
+}
