81d72e2af55fd29e2ea05709e6da12bcc629f22c
VelislavKarastoychev <exel_mmm@abv.bg>
2023-12-08 10:05:13 +0200

    Minimization of the message card

--- avclub-msngr/src/components/MessageComponent/MessageComponent.tsx
+++ avclub-msngr/src/components/MessageComponent/MessageComponent.tsx
@@ -1,19 +1,31 @@
 "use strict";
-import {BsThreeDotsVertical} from 'react-icons/bs';
-import {BiChat, BiLike, BiShare} from 'react-icons/bi'
-import { Box, Button ,Card, CardBody, CardHeader, CardFooter,Avatar, Flex, Heading, IconButton, Text } from "@chakra-ui/react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { BiChat, BiLike, BiShare } from "react-icons/bi";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
 import { DefaultUserData, MessageInfo } from "../../types/types";
 import { useContext, useEffect, useState } from "react";
 import { getUserByUid } from "../../services";
 import { UserContext } from "../../context/AuthContext";
-import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
-import 'froala-editor/js/plugins.pkgd.min.js';
+import "froala-editor/css/froala_style.min.css";
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/js/plugins.pkgd.min.js";
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
 import { froalaBioConfig } from "../../utils/profileUtils";
 
 export const MessageComponent = (
-  { message }: { message: MessageInfo }
+  { message }: { message: MessageInfo },
 ): JSX.Element => {
   const [user, setUser] = useState<DefaultUserData | null>(null);
   const { userData } = useContext(UserContext);
@@ -25,21 +37,22 @@ export const MessageComponent = (
   }, []);
   return (
     <Card
-    alignSelf={message.uid === userData?.uid ? 'flex-end' : 'flex-start'} 
-    maxW='md'
-    mb={2}
-    border={'1px solid'}
-    borderColor={'brand.accent'}
-    bgColor={'gray.50'}
+      p='0'
+      alignSelf={message.uid === userData?.uid ? "flex-end" : "flex-start"}
+      maxW="md"
+      mb={2}
+      border={"1px solid"}
+      borderColor={"brand.accent"}
+      bgColor={"gray.50"}
     >
-      <CardHeader >
+      <CardHeader py={2}>
         <Flex>
           <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
             <Avatar name={user?.username} src={user?.avatarUrl} />
 
             <Box>
               <Heading size="sm">{user?.username}</Heading>
-              <Text fontStyle={'italic'} color={'grey'} fontSize={'smaller'}>
+              <Text fontStyle={"italic"} color={"grey"} fontSize={"smaller"}>
                 {new Date(message.createdOn).toDateString()}
               </Text>
             </Box>
@@ -52,30 +65,19 @@ export const MessageComponent = (
           />
         </Flex>
       </CardHeader>
-      <CardBody>
-        {/* <Text>
-          {message.content}
-        </Text> */}
+      <CardBody py={1}>
         <FroalaEditorView model={message.content} />
       </CardBody>
       <CardFooter
+        py={1}
         justify="space-between"
         flexWrap="wrap"
         sx={{
           "& > button": {
-            minH: '12px'
+            minH: "12px",
           },
         }}
       >
-        <Button flex="1" variant="ghost" leftIcon={<BiLike />}>
-          Like
-        </Button>
-        <Button flex="1" variant="ghost" leftIcon={<BiChat />}>
-          Comment
-        </Button>
-        <Button flex="1" variant="ghost" leftIcon={<BiShare />}>
-          Share
-        </Button>
       </CardFooter>
     </Card>
   );
