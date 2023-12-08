ed450d12b4ee3ba99cff9e2f56610018f6bc2466
Andrey Kunev <akunev45@gmail.com>
2023-12-08 19:44:48 +0200

    Added channel list

---
+++ avclub-msngr/src/components/ChannelList/ChannelList.tsx
@@ -0,0 +1,22 @@
+import { ChatsCollection } from "../../types/types";
+import { Stack, Link, Box } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import { FC } from "react";
+
+export const ChannelList: FC<{ channelArr: ChatsCollection }> = ({ channelArr }) => {
+    return (
+        <Stack direction='column' mt={2}>
+            {channelArr.map(channel => (
+            <Box borderBottom='1px solid' borderColor='gray.100'>
+                <Link as={NavLink} key={channel.chatId}
+                    fontSize={20}
+                    fontWeight={600}
+                    _hover={{
+                        color: 'brand.accent',
+                    }}
+                >{channel.name}</Link>
+            </Box>
+            ))}
+        </Stack>
+    );
+};
