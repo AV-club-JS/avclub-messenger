fe4d53583d3c2c63130da459e5000b96314da95d
Andrey Kunev <akunev45@gmail.com>
2023-12-11 23:31:56 +0200

    Added meeting wrapper, video meeting and create room

---
+++ avclub-msngr/src/components/VideoMeeting/VideoMeeting.tsx
@@ -0,0 +1,24 @@
+import { Box, Text } from "@chakra-ui/react";
+import { DyteMeeting } from "@dytesdk/react-ui-kit";
+import { useDyteMeeting } from "@dytesdk/react-web-core";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const VideoMeeting = () => {
+    const { meeting } = useDyteMeeting();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        meeting.self.on('roomLeft', () => {
+            navigate('/chats');
+        })        
+    }, [meeting])
+
+
+    return (
+        <Box h={`calc(100vh - 60px)`}>
+            {meeting && <DyteMeeting mode="fill" meeting={meeting} />}
+            {!meeting && <Text>Loading....</Text>}
+        </Box>
+    );
+}
