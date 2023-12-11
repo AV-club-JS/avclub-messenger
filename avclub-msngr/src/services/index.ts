8f4baa5f1d8ede81397c848d6918df4b19fe43f3
Andrey Kunev <akunev45@gmail.com>
2023-12-11 23:26:35 +0200

    Added dyte service to index

--- avclub-msngr/src/services/index.ts
+++ avclub-msngr/src/services/index.ts
@@ -2,3 +2,4 @@ export * from './auth.services.ts';
 export * from './users.services.ts';
 export * from './teams.services.ts';
 export * from './chat.services.ts';
+export * from './dyte.services.ts'
