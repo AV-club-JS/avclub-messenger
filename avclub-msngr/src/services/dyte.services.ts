3d280746e2d04af1191af04a8a30e66330635f12
Andrey Kunev <akunev45@gmail.com>
2023-12-11 23:25:11 +0200

    Added dyte services and constants

---
+++ avclub-msngr/src/services/dyte.services.ts
@@ -0,0 +1,30 @@
+import { ref, update } from "firebase/database";
+import { DYTE_URL, ENCODED_STR } from "../constants/dyteConstants";
+import { CHANNELS } from "../constants/servicesConstants";
+import { db } from "../config/firebase-config";
+
+export const dyteRoomCreate = async (chatId: string) => {
+    const response = await fetch(`${DYTE_URL}/meetings`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            Authorization: `Basic ${ENCODED_STR}`,
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            title: chatId
+        })
+    });
+
+    return response;
+}
+
+export const addRoomID = async (chatId: string, roomId: string) => {
+    if (chatId && roomId) {
+        const updateData = {
+            [`${CHANNELS}/${chatId}/roomId`]: roomId,
+        };
+
+        await update(ref(db), updateData);
+    }
+};
