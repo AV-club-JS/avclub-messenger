e19ef9f7ec764e5a3b380e23e0ba5b9df3521b45
Andrey Kunev <79260100+AndreyKunev@users.noreply.github.com>
2023-12-12 16:26:59 +0200

    Delete dyte const

--- avclub-msngr/src/constants/dyteConstants.ts
+++
@@ -1,8 +0,0 @@
-
-export const ORG_ID = '931dee5f-2466-4c47-ae08-7c714ab6f5fa';
-
-export const DYTE_API_KEY = 'cbd9817eccba394746e9';
-
-export const DYTE_URL = 'https://api.dyte.io/v2';
-
-export const ENCODED_STR = btoa(`${ORG_ID}: ${DYTE_API_KEY}`);
