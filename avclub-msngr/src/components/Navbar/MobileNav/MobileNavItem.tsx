4c3f4570ca488bf11d6592e77342686e92cd18c8
Andrey Kunev <akunev45@gmail.com>
2023-12-12 14:36:33 +0200

    Made profile variable route, changed navbar

--- avclub-msngr/src/components/Navbar/MobileNav/MobileNavItem.tsx
+++ avclub-msngr/src/components/Navbar/MobileNav/MobileNavItem.tsx
@@ -6,16 +6,27 @@ import {
 } from '@chakra-ui/react';
 import { NavItem } from '../../../types/types';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../../context/AuthContext';
 
 export const MobileNavItem = ({ label, nav }: NavItem) => {
     const navigate = useNavigate();
+    const { userData } = useContext(UserContext);
+    
+    const handleNavigate = (label: string, nav: string) => {
+        if (label === 'Profile') {
+            navigate(`/${userData?.uid}`)
+        } else {
+            navigate(nav);
+        }
+    }
 
     return (
         <Stack spacing={4}>
             <Box
                 py={2}
                 as="a"
-                onClick={() => navigate(nav)}
+                onClick={() => handleNavigate(label, nav)}
                 justifyContent="space-between"
                 alignItems="center"
                 _hover={{
