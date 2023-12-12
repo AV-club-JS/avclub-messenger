995728a388c4a7182e290f3c8e1aebf63d4b5dff
VelislavKarastoychev <exel_mmm@abv.bg>
2023-12-12 22:19:49 +0200

    Reformated MeetingWrapper

--- avclub-msngr/src/components/MeetingWrapper/MeetingWrapper.tsx
+++ avclub-msngr/src/components/MeetingWrapper/MeetingWrapper.tsx
@@ -1,62 +1,59 @@
-import { useContext, useEffect, useState } from 'react';
-import { DyteProvider, useDyteClient } from '@dytesdk/react-web-core';
-import { VideoMeeting } from '../VideoMeeting';
-import { useParams } from 'react-router-dom';
-import { UserContext } from '../../context/AuthContext';
-import { ENCODED_STR, DYTE_URL } from '../../constants/dyteConstants';
-import { Loading } from '../Loading';
+import { useContext, useEffect, useState } from "react";
+import { DyteProvider, useDyteClient } from "@dytesdk/react-web-core";
+import { VideoMeeting } from "../VideoMeeting";
+import { useParams } from "react-router-dom";
+import { UserContext } from "../../context/AuthContext";
+import { DYTE_URL, ENCODED_STR } from "../../constants/dyteConstants";
+import { Loading } from "../Loading";
 
 export const MeetingWrapper = () => {
-    const [meeting, initMeeting] = useDyteClient();
-    const { roomId } = useParams();
-    const { userData } = useContext(UserContext);
-    const [token, setToken] = useState();
+  const [meeting, initMeeting] = useDyteClient();
+  const { roomId } = useParams();
+  const { userData } = useContext(UserContext);
+  const [token, setToken] = useState();
 
+  useEffect(() => {
+    if (roomId) {
+      const dyteReq = fetch(`${DYTE_URL}/meetings/${roomId}/participants`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Basic ${ENCODED_STR}`,
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          name: `${userData?.username}`,
+          picture: `${userData?.avatarUrl}`,
+          preset_name: "group_call_participant",
+          custom_participant_id: `${userData?.uid}`,
+        }),
+      });
 
+      let dyteRes;
+      let authData;
 
-    useEffect(() => {
-        if (roomId) {
+      (async () => {
+        dyteRes = await dyteReq;
+        authData = await dyteRes.json();
+        console.log(authData)
+        setToken(authData.data.token);
+      })();
 
-            const dyteReq = fetch(`${DYTE_URL}/meetings/${roomId}/participants`, {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json',
-                    Authorization: `Basic ${ENCODED_STR}`,
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify({
-                    name: `${userData?.username}`,
-                    picture: `${userData?.avatarUrl}`,
-                    preset_name: 'group_call_participant',
-                    custom_participant_id: `${userData?.uid}`
-                })
-            });
+      if (token) {
+        initMeeting({
+          authToken: token!,
+          defaults: {
+            audio: false,
+            video: false,
+          },
+        });
+      }
+    }
+  }, [token]);
 
-            let dyteRes;
-            let authData;
-
-            (async () => {
-                dyteRes = await dyteReq;
-                authData = await dyteRes.json();
-                setToken(authData.data.token);
-            })();
-
-            if (token) {
-                initMeeting({
-                    authToken: token!,
-                    defaults: {
-                        audio: false,
-                        video: false,
-                    },
-                });
-            }
-
-        }
-    }, [token]);
-    
-    return (
-        <DyteProvider value={meeting} fallback={<Loading />}>
-            <VideoMeeting />
-        </ DyteProvider>
-    );
-}
+  return (
+    <DyteProvider value={meeting} fallback={<Loading />}>
+      <VideoMeeting />
+    </DyteProvider>
+  );
+};
