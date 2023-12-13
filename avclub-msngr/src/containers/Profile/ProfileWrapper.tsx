6bf83721a23f64e8dc09c4d74850a85dae8e3a62
Andrey Kunev <akunev45@gmail.com>
2023-12-13 09:17:48 +0200

    Added check for userData in profile wrapper

--- avclub-msngr/src/containers/Profile/ProfileWrapper.tsx
+++ avclub-msngr/src/containers/Profile/ProfileWrapper.tsx
@@ -2,14 +2,19 @@ import { UserContext } from "../../context/AuthContext";
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Profile, ProfileView } from ".";
+import { Loading } from "../../components/Loading";
 
 export const ProfileWrapper = () => {
     const { userData } = useContext(UserContext);   
     const { userId } = useParams();
     
-    if (userData!.uid == userId) {
-        return <Profile />
+    if (userData) {
+        if (userData!.uid == userId) {
+            return <Profile />
+        } else {
+            return <ProfileView />
+        }
     } else {
-        return <ProfileView />
+        <Loading/>
     }
 }
